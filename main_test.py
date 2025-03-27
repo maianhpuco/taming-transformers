5c33672cf6b42f43372dc91ff247ac90f92df2e3
Mai Anh Vu <maianhuel@gmail.com>
2025-03-27 15:08:48 -0500

    update

--- main_test.py
+++ main_test.py
@@ -41,6 +41,103 @@ def reconstruct_with_vqgan(x, model):
     xrec = model.decode(z)
     return xrec
 
+# ---------- image reconstruction code : DECODER ONLY ------------ 
+
+import io
+import os, sys
+import requests
+import PIL
+from PIL import Image
+from PIL import ImageDraw, ImageFont
+import numpy as np
+
+import torch
+import torch.nn.functional as F
+import torchvision.transforms as T
+import torchvision.transforms.functional as TF
+
+from dall_e          import map_pixels, unmap_pixels, load_model
+from IPython.display import display, display_markdown
+
+font = ImageFont.truetype("/usr/share/fonts/truetype/liberation/LiberationSans-BoldItalic.ttf", 22)
+
+def download_image(url):
+    resp = requests.get(url)
+    resp.raise_for_status()
+    return PIL.Image.open(io.BytesIO(resp.content))
+
+
+def preprocess(img, target_image_size=256, map_dalle=True):
+    s = min(img.size)
+    
+    if s < target_image_size:
+        raise ValueError(f'min dim for image {s} < {target_image_size}')
+        
+    r = target_image_size / s
+    s = (round(r * img.size[1]), round(r * img.size[0]))
+    img = TF.resize(img, s, interpolation=PIL.Image.LANCZOS)
+    img = TF.center_crop(img, output_size=2 * [target_image_size])
+    img = torch.unsqueeze(T.ToTensor()(img), 0)
+    if map_dalle: 
+      img = map_pixels(img)
+    return img
+
+
+def reconstruct_with_dalle(x, encoder, decoder, do_preprocess=False):
+    # takes in tensor (or optionally, a PIL image) and returns a PIL image
+    if do_preprocess:
+        x = preprocess(x)
+    z_logits = encoder(x)
+    z = torch.argmax(z_logits, axis=1)
+    
+    print(f"DALL-E: latent shape: {z.shape}")
+    z = F.one_hot(z, num_classes=encoder.vocab_size).permute(0, 3, 1, 2).float()
+
+    x_stats = decoder(z).float()
+    x_rec = unmap_pixels(torch.sigmoid(x_stats[:, :3]))
+    x_rec = T.ToPILImage(mode='RGB')(x_rec[0])
+
+    return x_rec
+
+
+def stack_reconstructions(input, x0, x1, x2, x3, titles=[]):
+    assert input.size == x1.size == x2.size == x3.size
+    w, h = input.size[0], input.size[1]
+    img = Image.new("RGB", (5*w, h))
+    img.paste(input, (0,0))
+    img.paste(x0, (1*w,0))
+    img.paste(x1, (2*w,0))
+    img.paste(x2, (3*w,0))
+    img.paste(x3, (4*w,0))
+    for i, title in enumerate(titles):
+        ImageDraw.Draw(img).text((i*w, 0), f'{title}', (255, 255, 255)) # coordinates, text, color, font
+    return img 
+
+def reconstruction_pipeline(url, size=320):
+    x_dalle = preprocess(download_image(url), target_image_size=size, map_dalle=True)
+    x_vqgan = preprocess(download_image(url), target_image_size=size, map_dalle=False)
+    x_dalle = x_dalle.to(DEVICE)
+    x_vqgan = x_vqgan.to(DEVICE)
+    
+    print(f"input is of size: {x_vqgan.shape}")
+    x0 = reconstruct_with_vqgan(preprocess_vqgan(x_vqgan), model32x32)
+    x1 = reconstruct_with_vqgan(preprocess_vqgan(x_vqgan), model16384)
+    x2 = reconstruct_with_vqgan(preprocess_vqgan(x_vqgan), model1024)
+    x3 = reconstruct_with_dalle(x_dalle, encoder_dalle, decoder_dalle)
+    img = stack_reconstructions(custom_to_pil(preprocess_vqgan(x_vqgan[0])), x3, 
+                                custom_to_pil(x0[0]), custom_to_pil(x1[0]), 
+                                custom_to_pil(x2[0]), titles=titles)
+    
+    tmp_dir = './tmp_save/'
+    save_path = os.path.join(tmp_dir, "reconstructions.jpg") 
+    img.save(save_path) 
+    print("done save the image") 
+    print(f"Saved reconstruction to: {save_path}") 
+def load_dalle_models():
+    global encoder_dalle, decoder_dalle
+    encoder_dalle = load_model("https://cdn.openai.com/dall-e/encoder.pkl", DEVICE)
+    decoder_dalle = load_model("https://cdn.openai.com/dall-e/decoder.pkl", DEVICE)
+ 
 if __name__=='__main__':
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
     print(f"DEVICE: {DEVICE}") 
@@ -48,7 +145,8 @@ if __name__=='__main__':
     sys.path.append(".")
     import torch
     torch.set_grad_enabled(False)
-
+    load_dalle_models()  
+    
     DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") 
     
     config1024 = load_config("logs/vqgan_imagenet_f16_1024/configs/model.yaml", display=False)
@@ -58,3 +156,7 @@ if __name__=='__main__':
     # model16384 = load_vqgan(config16384, ckpt_path="logs/vqgan_imagenet_f16_16384/checkpoints/last.ckpt").to(DEVICE)
     config32x32 = load_config("logs/vqgan_gumbel_f8/configs/model.yaml", display=False)
     model32x32 = load_vqgan(config32x32, ckpt_path="logs/vqgan_gumbel_f8/checkpoints/last.ckpt", is_gumbel=True).to(DEVICE)   
+    
+    titles = ["input", "DALL-E", "VQGAN 32x32", "VQGAN 1024", "VQGAN 16384"] 
+    reconstruction_pipeline(url='https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iKIWgaiJUtss/v2/1000x-1.jpg', size=384) 
+ 
