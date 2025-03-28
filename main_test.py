21f1a176d9b304d556d92df4b924c2e673291f6f
Mai Anh Vu <maianhuel@gmail.com>
2025-03-28 14:48:41 -0500

    update

--- main_test.py
+++ main_test.py
@@ -71,7 +71,9 @@ def custom_to_pil(x):
 def reconstruct_with_vqgan(x, model):
     # could also use model(x) for reconstruction but use explicit encoding and decoding here
     z, _, [_, _, indices] = model.encode(x)
+    
     print(f"VQGAN --- {model.__class__.__name__}: latent shape: {z.shape[2:]}")
+    print("indices shape: ", indices.shape, "indices unique: ", torch.unique(indices).shape) 
     xrec = model.decode(z)
     return xrec
 
@@ -190,7 +192,8 @@ if __name__=='__main__':
     import torch
     torch.set_grad_enabled(False)
     load_dalle_models() 
-     
+    
+    
     config1024 = load_config("logs/vqgan_imagenet_f16_1024/configs/model.yaml", display=False)
     config16384 = load_config("logs/vqgan_imagenet_f16_16384/configs/model.yaml", display=False)
 
@@ -198,6 +201,8 @@ if __name__=='__main__':
     model16384 = load_vqgan(config16384, ckpt_path="logs/vqgan_imagenet_f16_16384/checkpoints/last.ckpt").to(DEVICE)
     config32x32 = load_config("logs/vqgan_gumbel_f8/configs/model.yaml", display=False)
     model32x32 = load_vqgan(config32x32, ckpt_path="logs/vqgan_gumbel_f8/checkpoints/last.ckpt", is_gumbel=True).to(DEVICE)   
+    
+    print("---model1024 codebook shape:", model1024.quantize.embedding.weight.shape)
     
     titles = ["input", "DALL-E", "VQGAN 32x32", "VQGAN 1024", "VQGAN 16384"] 
     
@@ -225,3 +230,23 @@ if __name__=='__main__':
         
     print("------> done")  
     #conda environments:
+
+
+
+# # download a VQGAN with f=16 (16x compression per spatial dimension) and with a codebook with 1024 entries
+# !mkdir -p logs/vqgan_imagenet_f16_1024/checkpoints
+# !mkdir -p logs/vqgan_imagenet_f16_1024/configs
+# !wget 'https://heibox.uni-heidelberg.de/f/140747ba53464f49b476/?dl=1' -O 'logs/vqgan_imagenet_f16_1024/checkpoints/last.ckpt' 
+# !wget 'https://heibox.uni-heidelberg.de/f/6ecf2af6c658432c8298/?dl=1' -O 'logs/vqgan_imagenet_f16_1024/configs/model.yaml' 
+
+# # download a VQGAN with f=16 (16x compression per spatial dimension) and with a larger codebook (16384 entries)
+# !mkdir -p logs/vqgan_imagenet_f16_16384/checkpoints
+# !mkdir -p logs/vqgan_imagenet_f16_16384/configs
+# !wget 'https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1' -O 'logs/vqgan_imagenet_f16_16384/checkpoints/last.ckpt' 
+# !wget 'https://heibox.uni-heidelberg.de/f/274fb24ed38341bfa753/?dl=1' -O 'logs/vqgan_imagenet_f16_16384/configs/model.yaml' 
+
+# # download a VQGAN with f=8 (8x compression per spatial dimension) and a larger codebook-size with 8192 entries
+# !mkdir -p logs/vqgan_gumbel_f8/checkpoints
+# !mkdir -p logs/vqgan_gumbel_f8/configs
+# !wget 'https://heibox.uni-heidelberg.de/f/34a747d5765840b5a99d/?dl=1' -O 'logs/vqgan_gumbel_f8/checkpoints/last.ckpt' 
+# !wget 'https://heibox.uni-heidelberg.de/f/b24d14998a8d4f19a34f/?dl=1' -O 'logs/vqgan_gumbel_f8/configs/model.yaml' 
